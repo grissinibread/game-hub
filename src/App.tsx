3ba226e88de869bc3109c8cf6501f358ca22b732
Alejandro Navarro <alexnavarro57@gmail.com>
2026-06-16 17:36:53 -0700

    Built a responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+const breakpoints = {
+  base: "0rem", // 0px
+  sm: "30rem", // ~480px
+  md: "48rem", // ~768px
+  lg: "63.9375rem", // ~1023px
+  xl: "80rem", // ~1280px
+  "2xl": "96rem", // ~1536px
+};
 
 function App() {
   return (
     <>
-      <Button colorPalette={'blue'}>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `'nav' 'main'`,
+          lg: `'nav nav' 'aside main'`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"coral"}>
+          Nav
+        </GridItem>
+
+        <GridItem area={"aside"} bg={"gold"} hideBelow={breakpoints.lg}>
+          Aside
+        </GridItem>
+
+        <GridItem area={"main"} bg={"dodgerblue"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
