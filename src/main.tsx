2deec09e58fa073486b57ad393aca022bbcfbab1
Alejandro Navarro <alexnavarro57@gmail.com>
2026-06-16 18:01:08 -0700

    Implemented dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <Provider defaultTheme="dark">
       <App />
     </Provider>
   </React.StrictMode>,
