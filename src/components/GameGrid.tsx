aaa6ab1af7a5174aadc75417b5ea837c60a3ce7b
Alejandro Navarro <alexnavarro57@gmail.com>
2026-07-15 01:23:30 -0700

    Refactored game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,59 +1,35 @@
-import useGames, { Platform } from "@/hooks/useGames";
+import useGames from "@/hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
-import jong from "../assets/jong.jpg";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "@/hooks/UseGenres";
-import usePlatforms from "@/hooks/usePlatforms";
-import { GameQuery } from "@/App";
+import * as App from "@/App";
 
 interface Props {
-  gameQuery: GameQuery;
+  gameQuery: App.GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6} padding="10px">
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6} padding="10px">
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
 export default GameGrid;
-
-{
-  /* <GameCard
-          game={{
-            background_image: jong,
-            name: "Escape Jong 3: You Can't",
-            id: 67,
-            parent_platforms: [
-              {
-                platform: {
-                  id: 69,
-                  name: "PoopStation",
-                  slug: "poop",
-                },
-              },
-            ],
-            metacritic: 67
-          }}
-        /> */
-}
